fb802839ef77db97aa35df56f1bc2f6fce246341
Jan Rolenc <jrolenc@centrum.cz>
2022-01-02 18:18:32 +0100

    new commit

--- server.js
+++ server.js
@@ -3,49 +3,53 @@ const cors = require('cors');
 
 const app = express();
 
-
-//bez techto dvou radku by hlasil error, ze nerozumi, co je body.input
-// TypeError: Cannot destructure property 'input' of 'req.body' as it is undefined.
+//bez techto dvou radku by hlasil error, ze nerozumi, co je body.password
+// TypeError: Cannot destructure property 'password' of 'req.body' as it is undefined.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('ahoj')
+    res.send(database.users)
 })
 
-const heslo = 'kkz';
+const database = {
+    users: [
+        { name: 'jan', email: 'jan@gmail.com', heslo: 'kkz' },
+        { name: 'jana', email: 'jana@gmail.com', heslo: 'jana' },
+        { name: 'janek', email: 'janek@gmail.com', heslo: 'janek' }
+    ]
+}
 
 app.post('/signin', (req, res) => {
-        const { input } = req.body;
-        if (!input) {
-            return res.status(400).json('the password is not filled in')
-        }
-        if (input === heslo) {
-            // return res.json('heslo is ok') //funguje s i bez return
-            return res.json('ok')
-        } else {
-            return res.status(400).json('error loggin in')
-        }
-    })
-    // app.post('/register', (req, res) => {
-    //     const { input } = req.body;
-    //     if (!input) {
-    //         res.status(400).json('the password is not filled in')
-    //     }
-    //     if (input === heslo) {
-    //         // return res.json('heslo is ok') //funguje s i bez return
-    //         res.json('ok')
-    //     } else {
-    //         res.status(400).json('error loggin in')
-    //     }
-    // })
+    const { password } = req.body;
+    if (!password) {
+        res.status(400).json('the password is not filled in');
+    } else if (password === database.users[0].heslo) {
+        // return res.json('heslo is ok') //funguje s i bez return
+        res.json('ok');
+    } else {
+        res.status(400).json('error loggin in');
+    }
+})
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    if ((!password) || (!name) || (!email)) {
+        res.status(400).json('some data is not filled in');
+    } else {
+        database.users.push({
+            name: name,
+            email: email,
+            heslo: password
+        });
+        res.json(database.users[database.users.length - 1]);
+    }
 
+})
 
 // app.listen(process.env.PORT || 3000, (req, res) => {
 //     console.log(`server is running on port ${process.env.PORT}`);
 // })
-const server = app.listen(process.env.PORT || 3000, () => {
-    const port = server.address().port;
-    console.log(`Express is working on port ${port}`);
-});
+app.listen(3000, (req, res) => {
+    console.log("server is running on port 3000");
+})
